e28fa0d0479da5703371c59f95546c323eeaf653
Tarokh Mohammadi <tarokhDevMoh2020@gmail.com>
2022-04-25 19:10:17 +0430

    Updates

---
+++ precache-manifest.6616d91d0723ee82abe8e451df9b06c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f51696f570a55ed91642ff22ccbcf46",
+    "url": "/tarokh-portfolio/index.html"
+  },
+  {
+    "revision": "03cf6f8603bd28a6f687",
+    "url": "/tarokh-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f9dc41bf6b0976ab086a",
+    "url": "/tarokh-portfolio/static/css/main.0fe788ec.chunk.css"
+  },
+  {
+    "revision": "03cf6f8603bd28a6f687",
+    "url": "/tarokh-portfolio/static/js/2.44ae7e6a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/tarokh-portfolio/static/js/2.44ae7e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9dc41bf6b0976ab086a",
+    "url": "/tarokh-portfolio/static/js/main.36ab2405.chunk.js"
+  },
+  {
+    "revision": "8dd2ef59729814f4b358",
+    "url": "/tarokh-portfolio/static/js/runtime-main.56a7cbe4.js"
+  }
+]);
